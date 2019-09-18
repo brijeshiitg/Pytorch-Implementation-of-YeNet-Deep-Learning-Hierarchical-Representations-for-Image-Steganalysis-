b569d64765b738a86a1031d61320ea355ed1d072
Brijesh Singh <brijesh.singh@iitg.ac.in>
2019-09-18 17:04:57 +0530

    Update Yenet.py

--- Yenet.py
+++ Yenet.py
@@ -10,14 +10,13 @@ srm_minmax = np.load('./minmax_filters.npy')
 srm_filters = np.concatenate((srm_16filters, srm_minmax),axis=0)
 
 srm_filters = torch.from_numpy(srm_filters).to(device=device, dtype=torch.float)
+srm_filters = torch.autograd.Variable(srm_filters, requires_grad=True)
 # print(srm_filters.shape)
 
 class Yenet(nn.Module):
 	def __init__(self):
 		super(Yenet, self).__init__()
 
-
-		# self.conv1 = nn.Conv2d(1, 30, kernel_size=5, stride=1, padding=0)
 		self.tlu = nn.Hardtanh(min_val=-3.0, max_val=3.0)
 
 		self.conv2 = nn.Conv2d(30, 30, kernel_size=3, stride=1, padding=0)
@@ -41,27 +40,18 @@ class Yenet(nn.Module):
 		
 	def forward(self, x):
 		out = self.tlu(F.conv2d(x, srm_filters))
-		# print('layer 1',out.shape)
 		out = F.relu(self.conv2(out))
-		# print('layer 2',out.shape)
 		out = F.relu(self.conv3(out))
-		# print('layer 3',out.shape)
 		out = F.relu(self.conv4(out))
 		out = F.avg_pool2d(out, kernel_size=3, stride=2, padding=1)
-		# print('layer 4',out.shape)
 		out = F.relu(self.conv5(out))
 		out = F.avg_pool2d(out, kernel_size=3, stride=2, padding=0)
-		# print('layer 5',out.shape)
 		out = F.relu(self.conv6(out))
 		out = F.avg_pool2d(out, kernel_size=3, stride=2, padding=0)
-		# print('layer 6',out.shape)
 		out = F.relu(self.conv7(out))
 		out = F.avg_pool2d(out, kernel_size=3, stride=2, padding=0)
-		# print('layer 7',out.shape)
 		out = F.relu(self.conv8(out))
-		# print('layer 8',out.shape)
 		out = F.relu(self.conv9(out))
-		# print('layer 9',out.shape)
 		out = out.view(out.size(0), -1)
 		out = self.fc(out)
 		return out
